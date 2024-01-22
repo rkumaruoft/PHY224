a8511e720e5ede6dc91de934ea1924c4d919af3a
rkumaruoft <reeshav.kumar@mail.utoronto.ca>
2024-01-22 18:01:26 -0500

    Ohms law pythoion

--- Ohms and Powers lab/Ohm's Law.py
+++ Ohms and Powers lab/Ohm's Law.py
@@ -4,12 +4,16 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 
-def func_ohm_law(x, r):
-    return x / r
+def func_ohm_law(x, I):
+    return x / I
+
+
+def linear_model(x, a, b):
+    return (a * x) + b
 
 
 if __name__ == "__main__":
-    data = numpy.loadtxt("part1_resistor_data.csv", delimiter=',')
+    data = numpy.loadtxt("voltage-current-data-part1.csv", delimiter=',')
 
     voltage_data = []
     current_data = []
@@ -21,3 +25,18 @@ if __name__ == "__main__":
         current_data.append(line[1])
         voltage_uncert.append(line[2])
         current_uncert.append(line[3])
+
+    voltage_data = numpy.array(voltage_data)
+    current_data = numpy.array(current_data)
+    voltage_uncert = numpy.array(voltage_uncert)
+    current_uncert = numpy.array(current_uncert)
+    print(voltage_data)
+    print(current_data)
+    popt, pcov = curve_fit(linear_model, xdata=current_data, ydata=voltage_data)
+    print(popt)
+    plt.scatter(current_data, voltage_data)
+    plt.xlabel("Current (A)")
+    plt.ylabel("Voltage (V)")
+    plt.title("Ohm's Law")
+    plt.rcParams["figure.dpi"] = 900
+    plt.show()
