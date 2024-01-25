2e12a7c1ca5a6859cdfe08115b0d78404341e930
rkumaruoft <reeshav.kumar@mail.utoronto.ca>
2024-01-24 22:37:11 -0500

    bulb.py

--- Ohms and Powers lab/Code/Bulb.py
+++ Ohms and Powers lab/Code/Bulb.py
@@ -1,16 +1,23 @@
+import math
+
 import matplotlib as mat
 import numpy
 import matplotlib.pyplot as plt
+import pylab
 from scipy.optimize import curve_fit
-from functions import linear_model,
+from functions import linear_model
 
 
-def power_law(voltage, resistance): # TODO
-    return voltage / resistance
+def power_law(voltage, prop_constant):  # TODO
+    return prop_constant * (voltage**(3/5))
 
 
 def power_model(x, c, d):
-    return c*x**d
+    return c * x ** d
+
+
+def linear_log_model(x, a, b):
+    return b * math.log(x) + math.log(a)
 
 
 def x_r2_metric(N, m, measured_current_data, voltage_data, a, b, uncertainties):
@@ -20,13 +27,13 @@ def x_r2_metric(N, m, measured_current_data, voltage_data, a, b, uncertainties):
     return sum / (N - m)
 
 
-def main_func(data):
+if __name__ == "__main__":
+    data = numpy.loadtxt("../voltage_current_data_bulb_part3.csv", delimiter=',')
     voltage_data = []
     current_data = []
     voltage_uncert = []
     current_uncert = []
     # for linear regression on the linear model
-
 
     i = 0
     for line in data:
@@ -35,29 +42,33 @@ def main_func(data):
         voltage_uncert.append(line[2])
         current_uncert.append(line[3])
 
+    log_voltage_data = numpy.array([math.log(x) for x in voltage_data])
+    log_current_data = numpy.array([math.log(x) for x in current_data])
     voltage_data = numpy.array(voltage_data)
     current_data = numpy.array(current_data)
     voltage_uncert = numpy.array(voltage_uncert)
     current_uncert = numpy.array(current_uncert)
 
     plt.errorbar(voltage_data, current_data, yerr=current_uncert, xerr=voltage_uncert, fmt=" ")
-    popt, pcov = curve_fit(linear_model, xdata=voltage_data, ydata=current_data, p0=[1 / 470, 0]) # logarithm on the data
+    #TODO curve fit log_x and log_y
+    # popt, pcov = curve_fit(linear_model, xdata=log_voltage_data, ydata=log_current_data)  # logarithm on the data
+    # print(popt)
+    # # # b^Log_b(K) = k
+    # # # do math to convert log model to power formula
+    # # # y=cx^d , log(y) = dlog(x) + log(c)
+    # # from curve fitting the log of x and y data;
+    # # log(y)=a log(x)+b
+    # # a=d, b=log(c)
+    # a = popt[0]
+    # # plt.plot(log_voltage_data, fit_data)
+    # fit_data = powe(voltage_data, a)
+    popt, pcov = curve_fit(power_model, xdata=voltage_data, ydata=current_data)
     print(popt)
-    fit_data = func_ohm_law(voltage_data, 1 / popt[0])
-    plt.plot(voltage_data, fit_data, label='Curve Fit (y = mx +b)')
-    x_r2 = x_r2_metric(len(current_data), 2, current_data, voltage_data, popt[0], popt[1], current_uncert)
-    print(x_r2)
-    popt, pcov = curve_fit(linear_model_2, xdata=voltage_data, ydata=current_data, p0=[1 / 470]) # curve fit the power model
-    fit_data = func_ohm_law(voltage_data, 1 / popt[0])
-    plt.plot(voltage_data, fit_data, label='Curve Fit (y = mx)', linestyle='dashed', color='blue')
+    model_data = power_model(voltage_data,popt[0],popt[1])
+    plt.plot(voltage_data, model_data)
     plt.xlabel("Voltage (V)")
     plt.ylabel("Current (A)")
     plt.title("Ohm's Law")
     plt.legend()
     plt.rcParams["figure.dpi"] = 900
     plt.show()
-
-
-if __name__ == "__main__":
-    data = numpy.loadtxt("voltage-current-data_bulb-part3.csv", delimiter=',')
-    main_func(data)
