6b9f35805694acdfe7af2ecaf0215ce444602ed3
rkumaruoft <reeshav.kumar@mail.utoronto.ca>
2024-01-25 22:59:43 -0500

    bulb.py

--- Ohms and Powers lab/Code/Bulb_log_log.py
+++ Ohms and Powers lab/Code/Bulb_log_log.py
@@ -1,33 +1,11 @@
 import math
-
 import matplotlib as mat
 import numpy
 import matplotlib.pyplot as plt
 import pylab
 from scipy.optimize import curve_fit
 from functions import linear_model
-
-
-def theoretical_model(voltage, prop_constant):
-    return prop_constant * (voltage ** (3 / 5))
-
-
-def power_model(x, c, d):
-    return c * (x ** d)
-
-
-def x_r2_metric(N, m, measured_current_data, calculated_data, uncertainties):
-    sum = 0
-    for i in range(N):
-        sum += ((measured_current_data[i] - calculated_data[i]) ** 2) / (uncertainties[i] ** 2)
-    return sum / (N - m)
-
-
-def x_r2_metric_power(N, m, measured_current_data, voltage_data, c, d, uncertainties):
-    sum = 0
-    for i in range(N):
-        sum += (((measured_current_data[i] - power_model(voltage_data[i], c, d)) ** 2) / (uncertainties[i] ** 2))
-    return sum / (N - m)
+from Bulb import theoretical_model, power_model
 
 
 if __name__ == "__main__":
@@ -37,6 +15,7 @@ if __name__ == "__main__":
     voltage_uncert = []
     current_uncert = []
     # for linear regression on the linear model
+
     i = 0
     for line in data:
         voltage_data.append(line[0])
@@ -52,8 +31,6 @@ if __name__ == "__main__":
     current_uncert = numpy.array(current_uncert)
 
     plt.errorbar(voltage_data, current_data, yerr=current_uncert, xerr=voltage_uncert, fmt=" ")
-
-    # LOG MODEL CALCULATIONS
     popt, pcov = curve_fit(linear_model, xdata=log_voltage_data, ydata=log_current_data)
     print("Popt of log model: ", popt)
     log_model_data = linear_model(log_voltage_data, popt[0], popt[1])
@@ -61,32 +38,19 @@ if __name__ == "__main__":
     for line in log_model_data:
         antilog_model_data.append(math.exp(line))
     plt.plot(voltage_data, antilog_model_data, linestyle='dotted', color='black', label='Log Linear regression')
-    # X_r_linear metric calculations
-    x_r_linear = x_r2_metric(len(log_current_data), 2, current_data, antilog_model_data, current_uncert)
-    print(x_r_linear)
-
-    # POWER MODEL CALCULATIONS
     popt, pcov = curve_fit(power_model, xdata=voltage_data, ydata=current_data)
-    print("Popt of powermodel:" + str(popt))
+    print(popt)
     model_data = power_model(voltage_data, popt[0], popt[1])
-    plt.plot(voltage_data, model_data, label='Nonlinear Regression')
-    # X-r_power metric calculations
-    x_r_power = x_r2_metric(len(current_data), 2,current_data, model_data, current_uncert)
-    print(x_r_power)
-    # Theoretical Model Calculations
     popt, pcov = curve_fit(theoretical_model, xdata=voltage_data, ydata=current_data)
-    print("Popt of thoretical model" + str(popt))
+    print(popt)
     theoretical_data = theoretical_model(voltage_data, popt[0])
-    plt.plot(voltage_data, theoretical_data, linestyle='dashed', color='blue', label='Theoretical Curve')
-
-    # Co-Variance calculations
-
-
-
-    print(pcov)
-    plt.xlabel("Voltage (V)")
-    plt.ylabel("Current (A)")
-    plt.title("Ohm's Law")
+    plt.plot(voltage_data, model_data)
+    plt.plot(voltage_data, theoretical_data, linestyle='dashed', color='blue')
+    pylab.plt.xscale('log')
+    pylab.plt.yscale('log')
+    plt.xlabel("log(Voltage) (V)")
+    plt.ylabel("log(Current) (A)")
+    plt.title("Bulb Data")
     plt.legend()
     plt.rcParams["figure.dpi"] = 900
     plt.show()
