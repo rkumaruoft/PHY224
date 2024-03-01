14f0e6dd76ead89e7868389844663faf61bde04f
Arthur <arthurximing.li@mail.utoronto.ca>
2024-03-01 15:57:29 -0500

    Lab4-Data

--- LAB4-LCR/Code/RC_Circuit.py
+++ LAB4-LCR/Code/RC_Circuit.py
@@ -7,9 +7,9 @@ from scipy.optimize import curve_fit
 from functions import *
 
 if __name__ == '__main__':
-
+    # Part 1
     # RESISTOR DATA
-    rc_resistance_data = read_file_data("../scope_1_resistence.csv")
+    rc_resistance_data = read_file_data("../RC_circuit_battery_resistor_ex1.csv")
     # time data is in seconds and voltage data is in Volts
     time_data = []
     voltage_data = []
@@ -20,9 +20,11 @@ if __name__ == '__main__':
         voltage_data.append(this_line[1])
 
     plt.errorbar(time_data, voltage_data, fmt=".", label="Resistance Data")
-
+    plt.legend("Resistor")
+    plt.title("Resistor")
+    plt.show()
     # CAPACITOR DATA
-    rc_capacitor_data = read_file_data("../scope_2_capacitor.csv")
+    rc_capacitor_data = read_file_data("../RC_circuit_battery_capacitor_ex1.csv")
     time_data = []
     voltage_data = []
     for line in rc_capacitor_data:
@@ -34,5 +36,17 @@ if __name__ == '__main__':
     plt.errorbar(time_data, voltage_data, fmt=".", label="Capacitor Data")
     plt.xlabel("Time")
     plt.ylabel("Voltage")
-    plt.legend()
+    plt.legend("Capacitor")
+    plt.title("Capacitor")
     plt.show()
+
+    # Part 2
+
+    draw_data("RC_circuit_capacitor_ex2.csv", "RC capacitor ex2", "ex")
+    draw_data("RC_circuit_resistor_ex2.csv", "RC resistor ex2", "ex")
+
+    draw_data("RL_circuit_inductor_ex2.csv", "RL inductor ex2", "ex")
+    draw_data("RL_circuit_resistor_ex2.csv", "RL resistor ex2", "ex")
+
+    draw_data("LC_circuit_capacitor_ex2.csv", "LC capacitor ex2", "ex")
+    draw_data("LC_circuit_inductor_ex2.csv", "LC inductor ex2", "ex")
--- LAB4-LCR/Code/functions.py
+++ LAB4-LCR/Code/functions.py
@@ -1,6 +1,23 @@
+import matplotlib.pyplot as plt
 def read_file_data(filename):
     file = open(filename, 'r')
     lines = file.readlines()
     lines.pop(0)
     lines.pop(0)
     return lines
+
+def draw_data(data, title, legend):
+    data = read_file_data("../" + data)
+    # time data is in seconds
+    x_data = []
+    y_data = []
+    for line in data:
+        line.replace("\n", '')
+        this_line = [float(num) for num in line.split(",")]
+        x_data.append(this_line[0])
+        y_data.append(this_line[1])
+
+    plt.errorbar(x_data, y_data, fmt=".", label="Resistance Data")
+    plt.legend(legend)
+    plt.title(title)
+    plt.show()
