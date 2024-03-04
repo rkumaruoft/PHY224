fe87f00a85d6f71abdc869e546d037a3a8c924cf
Arthur <arthurximing.li@mail.utoronto.ca>
2024-03-04 18:00:48 -0500

    Lab4-Data

--- LAB4-LCR/Code/RC_Circuit.py
+++ LAB4-LCR/Code/RC_Circuit.py
@@ -8,22 +8,10 @@ from functions import *
 
 if __name__ == '__main__':
     # Part 1
-    # RESISTOR DATA
-    rc_resistance_data = read_file_data("../RC_circuit_battery_resistor_ex1.csv")
-    # time data is in seconds and voltage data is in Volts
-    time_data = []
-    voltage_data = []
-    for line in rc_resistance_data:
-        line.replace("\n", '')
-        this_line = [float(num) for num in line.split(",")]
-        time_data.append(this_line[0])
-        voltage_data.append(this_line[1])
 
-    plt.errorbar(time_data, voltage_data, fmt=".", label="Resistance Data")
-    plt.legend("Resistor")
-    plt.title("Resistor")
-    plt.show()
-    # CAPACITOR DATA
+
+
+    # # CAPACITOR DATA
     rc_capacitor_data = read_file_data("../RC_circuit_battery_capacitor_ex1.csv")
     time_data = []
     voltage_data = []
@@ -38,15 +26,62 @@ if __name__ == '__main__':
     plt.ylabel("Voltage")
     plt.legend("Capacitor")
     plt.title("Capacitor")
+
+    plt.axhline(y=0)
+
+    # skip the exponential increase sections
+    #curve 1
+    draw_curve(-4.283, -3.438, time_data, voltage_data, RC_eqn_V_c)
+    #curve 2
+    draw_curve(-2.585, -1.7, time_data, voltage_data, RC_eqn_V_c)
+    #curve 3
+    draw_curve(-0.9,-0.01, time_data, voltage_data, RC_eqn_V_c)
+    # curve 4
+    draw_curve(0.75, 1.64, time_data, voltage_data, RC_eqn_V_c)
+    # curve 5
+    draw_curve(2.856, 3.77, time_data, voltage_data, RC_eqn_V_c)
+
+    # time_data_intervals = []
+    # an_interval = []
+    # for i in range(len(time_data)):
+    #     an_interval.append(time_data[i])
+    #     if
+    #     if voltage_data[i+1] - voltage_data[i] >0.8:
+    #         time_data_intervals.append(an_interval)
+    #         an_interval = []
+
+    # for n in time_data_intervals:
+    #     draw_curve_2(n, voltage_data)
+    #
+
+
+
     plt.show()
+
+
+
+    # draw_data_and_curves("RC_circuit_battery_resistor_ex1.csv", "RC resistor ex1",
+                         #['TBD'], RC_function, [],False)
 
     # Part 2
 
-    draw_data("RC_circuit_capacitor_ex2.csv", "RC capacitor ex2", "ex")
+    # draw_data("RC_circuit_capacitor_ex2.csv", "RC capacitor ex2", "ex")
+    #curve 1
+    # draw_curve()
     draw_data("RC_circuit_resistor_ex2.csv", "RC resistor ex2", "ex")
+    time_data, voltage_data = data_to_xy("RC_circuit_resistor_ex2.csv")
 
-    draw_data("RL_circuit_inductor_ex2.csv", "RL inductor ex2", "ex")
+    draw_curve(-0.0925, -0.074, time_data, voltage_data, RC_eqn_V_r)
+
+    draw_curve(-0.0554, -0.0371, time_data, voltage_data, RC_eqn_V_r)
+
+    draw_curve(-0.0554, -0.0371, time_data, voltage_data, RC_eqn_V_r)
+    plt.show()
+    #
+    # draw_data("RL_circuit_inductor_ex2.csv", "RL inductor ex2", "ex")
     draw_data("RL_circuit_resistor_ex2.csv", "RL resistor ex2", "ex")
-
+    plt.show()
+    #
     draw_data("LC_circuit_capacitor_ex2.csv", "LC capacitor ex2", "ex")
     draw_data("LC_circuit_inductor_ex2.csv", "LC inductor ex2", "ex")
+    plt.show()
--- LAB4-LCR/Code/functions.py
+++ LAB4-LCR/Code/functions.py
@@ -1,4 +1,11 @@
+import math
+import matplotlib as mat
+import numpy as np
 import matplotlib.pyplot as plt
+import pylab
+from scipy.optimize import curve_fit
+
+
 def read_file_data(filename):
     file = open(filename, 'r')
     lines = file.readlines()
@@ -6,7 +13,8 @@ def read_file_data(filename):
     lines.pop(0)
     return lines
 
-def draw_data(data, title, legend):
+
+def data_to_xy(data):
     data = read_file_data("../" + data)
     # time data is in seconds
     x_data = []
@@ -16,8 +24,68 @@ def draw_data(data, title, legend):
         this_line = [float(num) for num in line.split(",")]
         x_data.append(this_line[0])
         y_data.append(this_line[1])
+    return x_data, y_data
+
+
+def draw_data(data, title, legend):
+    x_data, y_data = data_to_xy(data)
 
     plt.errorbar(x_data, y_data, fmt=".", label="Resistance Data")
     plt.legend(legend)
     plt.title(title)
-    plt.show()
+
+
+def draw_curve(x_start, x_end, x_data, y_data, function):
+    this_arr_x = []
+    this_arr_y = []
+    for i in range(len(x_data)):
+        if x_start <= x_data[i] <= x_end:
+            this_arr_x.append(x_data[i])
+            this_arr_y.append(y_data[i])
+    this_arr_x = np.array(this_arr_x)
+    this_arr_y = np.array(this_arr_y)
+
+    popt, pcov = curve_fit(function, xdata= this_arr_x, ydata=this_arr_y, maxfev=10000)
+    curve_data = function(this_arr_x, popt[0], popt[1], popt[2])
+    plt.plot(this_arr_x, curve_data)
+    print(popt[0])
+    print(popt[1])
+    print(popt[2])
+
+"""def draw_curve_2(time_interval, y_data):
+    this_arr_y = []
+    for i in range(len(time_interval)):
+        this_arr_y.append(y_data[i])
+    this_arr_y = np.array(this_arr_y)
+
+    popt, pcov = curve_fit(RC_eqn, xdata= time_interval, ydata=this_arr_y, maxfev=10000)
+    curve_data = RC_eqn(time_interval, popt[0], popt[1], popt[2])
+    plt.plot(time_interval, curve_data)
+    print(popt[0])
+    print(popt[1])
+    print(popt[2])"""
+
+def RC_eqn_V_c(t, a, time_const, b):
+    # time const = 1/RC
+    #b=RC
+    # v_in=1.468 volts
+    return a * (1/2)**(t * time_const) + b * (1.468)
+
+
+def RC_eqn_V_r(t, a, time_const, _b):
+    return 1.468*_b - a * (1/2)**(t*time_const)
+
+
+def RL_eqn_V_r(t, v_input, a, time_const):
+    # time const = R/L
+    # Resistance = 503 ohm
+    return (503)*a*(1/2)**(-t*time_const) + v_input
+
+
+def LC_eqn_V_c(t, v_input, a, b, time_const):
+
+    # time const = 1/sqrt(LC)
+    return a*math.sin(t*time_const) + b*math.cos(t*time_const) + v_input
+
+def LC_eqn_V_l():
+    return 1
