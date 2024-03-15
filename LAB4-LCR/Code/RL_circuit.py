af330476f32e820a8946c316a330c40a7cfa5382
Arthur <arthurximing.li@mail.utoronto.ca>
2024-03-14 22:07:57 -0400

    Lab4-Data

--- LAB4-LCR/Code/RL_circuit.py
+++ LAB4-LCR/Code/RL_circuit.py
@@ -70,8 +70,141 @@ if __name__ == '__main__':
     tau_errors.append(math.sqrt(pcov[1][1]))
     plt.plot(fall_1_time_data, fall_1_fit_data)
 
+    # rise 1 from 1.501 - 1.97
+    rise_1_time_data = []
+    rise_1_voltage_data = []
+    for index in range(len(time_data)):
+        if 1.501 <= time_data[index] <= 1.97:
+            rise_1_time_data.append(time_data[index])
+            rise_1_voltage_data.append(voltage_data[index])
+
+    rise_1_voltage_data = np.array(rise_1_voltage_data)
+    min_time_rise_1 = min(rise_1_time_data)
+    rise_1_time_data_scaled = np.array([(t - min_time_rise_1) for t in rise_1_time_data])
+    popt, pcov = curve_fit(RL_rise_equation, rise_1_time_data_scaled, rise_1_voltage_data)
+    rise_1_fit_data = RL_rise_equation(rise_1_time_data_scaled, popt[0], popt[1])
+    tau_values.append(popt[1])
+    tau_errors.append(math.sqrt(pcov[1][1]))
+    plt.plot(rise_1_time_data, rise_1_fit_data)
+
+    # fall 1 from 1.996 - 2.484
+    fall_1_time_data = []
+    fall_1_voltage_data = []
+    for index in range(len(time_data)):
+        if 1.996 <= time_data[index] <= 2.484:
+            fall_1_time_data.append(time_data[index])
+            fall_1_voltage_data.append(voltage_data[index])
+
+    fall_1_voltage_data = np.array(fall_1_voltage_data)
+    min_time_fall_1 = min(fall_1_time_data)
+    fall_1_time_data_scaled = np.array([(t - min_time_fall_1) for t in fall_1_time_data])
+    popt, pcov = curve_fit(RL_fall_equation, fall_1_time_data_scaled, fall_1_voltage_data)
+    fall_1_fit_data = RL_fall_equation(fall_1_time_data_scaled, popt[0], popt[1])
+    tau_values.append(popt[1])
+    tau_errors.append(math.sqrt(pcov[1][1]))
+    plt.plot(fall_1_time_data, fall_1_fit_data)
+
+
+    # rise 1 from 2.502 - 2.986
+    rise_1_time_data = []
+    rise_1_voltage_data = []
+    for index in range(len(time_data)):
+        if 2.502 <= time_data[index] <= 2.986:
+            rise_1_time_data.append(time_data[index])
+            rise_1_voltage_data.append(voltage_data[index])
+
+    rise_1_voltage_data = np.array(rise_1_voltage_data)
+    min_time_rise_1 = min(rise_1_time_data)
+    rise_1_time_data_scaled = np.array([(t - min_time_rise_1) for t in rise_1_time_data])
+    popt, pcov = curve_fit(RL_rise_equation, rise_1_time_data_scaled, rise_1_voltage_data)
+    rise_1_fit_data = RL_rise_equation(rise_1_time_data_scaled, popt[0], popt[1])
+    tau_values.append(popt[1])
+    tau_errors.append(math.sqrt(pcov[1][1]))
+    plt.plot(rise_1_time_data, rise_1_fit_data)
+
+
+    # fall 1 from 2.997 - 3.492
+    fall_1_time_data = []
+    fall_1_voltage_data = []
+    for index in range(len(time_data)):
+        if 2.997 <= time_data[index] <= 3.492:
+            fall_1_time_data.append(time_data[index])
+            fall_1_voltage_data.append(voltage_data[index])
+
+    fall_1_voltage_data = np.array(fall_1_voltage_data)
+    min_time_fall_1 = min(fall_1_time_data)
+    fall_1_time_data_scaled = np.array([(t - min_time_fall_1) for t in fall_1_time_data])
+    popt, pcov = curve_fit(RL_fall_equation, fall_1_time_data_scaled, fall_1_voltage_data)
+    fall_1_fit_data = RL_fall_equation(fall_1_time_data_scaled, popt[0], popt[1])
+    tau_values.append(popt[1])
+    tau_errors.append(math.sqrt(pcov[1][1]))
+    plt.plot(fall_1_time_data, fall_1_fit_data)
+
+
+
+    # rise 1 from 3.503 - 3.976
+    rise_1_time_data = []
+    rise_1_voltage_data = []
+    for index in range(len(time_data)):
+        if 3.503 <= time_data[index] <= 3.976:
+            rise_1_time_data.append(time_data[index])
+            rise_1_voltage_data.append(voltage_data[index])
+
+    rise_1_voltage_data = np.array(rise_1_voltage_data)
+    min_time_rise_1 = min(rise_1_time_data)
+    rise_1_time_data_scaled = np.array([(t - min_time_rise_1) for t in rise_1_time_data])
+    popt, pcov = curve_fit(RL_rise_equation, rise_1_time_data_scaled, rise_1_voltage_data)
+    rise_1_fit_data = RL_rise_equation(rise_1_time_data_scaled, popt[0], popt[1])
+    tau_values.append(popt[1])
+    tau_errors.append(math.sqrt(pcov[1][1]))
+    plt.plot(rise_1_time_data, rise_1_fit_data)
+
+    # fall 1 from 2.997 - 3.492
+    fall_1_time_data = []
+    fall_1_voltage_data = []
+    for index in range(len(time_data)):
+        if 2.997 <= time_data[index] <= 3.492:
+            fall_1_time_data.append(time_data[index])
+            fall_1_voltage_data.append(voltage_data[index])
+
+    fall_1_voltage_data = np.array(fall_1_voltage_data)
+    min_time_fall_1 = min(fall_1_time_data)
+    fall_1_time_data_scaled = np.array([(t - min_time_fall_1) for t in fall_1_time_data])
+    popt, pcov = curve_fit(RL_fall_equation, fall_1_time_data_scaled, fall_1_voltage_data)
+    fall_1_fit_data = RL_fall_equation(fall_1_time_data_scaled, popt[0], popt[1])
+    tau_values.append(popt[1])
+    tau_errors.append(math.sqrt(pcov[1][1]))
+    plt.plot(fall_1_time_data, fall_1_fit_data)
+
+    # fall 1 from 3.995 - 4.48
+    fall_1_time_data = []
+    fall_1_voltage_data = []
+    for index in range(len(time_data)):
+        if 3.995 <= time_data[index] <= 4.48:
+            fall_1_time_data.append(time_data[index])
+            fall_1_voltage_data.append(voltage_data[index])
+
+    fall_1_voltage_data = np.array(fall_1_voltage_data)
+    min_time_fall_1 = min(fall_1_time_data)
+    fall_1_time_data_scaled = np.array([(t - min_time_fall_1) for t in fall_1_time_data])
+    popt, pcov = curve_fit(RL_fall_equation, fall_1_time_data_scaled, fall_1_voltage_data)
+    fall_1_fit_data = RL_fall_equation(fall_1_time_data_scaled, popt[0], popt[1])
+    tau_values.append(popt[1])
+    tau_errors.append(math.sqrt(pcov[1][1]))
+    plt.plot(fall_1_time_data, fall_1_fit_data)
 
     plt.axhline(y=0)
     plt.xlabel("Time (milliseconds)")
     plt.ylabel("Voltage (volts)")
     plt.show()
+
+
+    print(tau_values)
+    tau_mean = 0
+    tau_error = 0
+    for i in range(len(tau_values)):
+        tau_mean += tau_values[2] # changes
+        tau_error += tau_errors[3] ** 2
+
+    print("mean of tau", tau_mean)
+    print("error of tau", 1 / len(tau_values) * np.sqrt(tau_error))
--- LAB4-LCR/Code/functions.py
+++ LAB4-LCR/Code/functions.py
@@ -55,7 +55,7 @@ def draw_curve(x_start, x_end, x_data, y_data, function):
     plt.plot(this_arr_x, curve_data)
     print(popt[0])
     print(popt[1])
-    return popt[0], popt[1]
+    return popt[0], pcov[0][0], popt[1], pcov[1][1]
 
 
 def draw_curve_3(x_start, x_end, x_data, y_data, function):
