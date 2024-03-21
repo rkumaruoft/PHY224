160945204303d6c18558c662aedb7a93871fb8ab
Arthur <arthurximing.li@mail.utoronto.ca>
2024-03-21 18:42:49 -0400

    Lab5-Data

--- Interference and Diffracttion/functions.py
+++ Interference and Diffracttion/functions.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pylab
 from scipy.optimize import curve_fit
 import numpy as np
+import math
 """Light Interference and diffraction lab"""
 
 def read_file_data(filename):
@@ -19,7 +20,6 @@ def data_to_xy(data):
     for line in data:
         line.replace("\t", '') # replace white space
         line.replace("\n", '') # replace line jump
-        print(line)
         this_line = [float(num) for num in line.split()]
         x_data.append(this_line[0])
         y_data.append(this_line[1])
@@ -28,6 +28,7 @@ def data_to_xy(data):
 
 def draw_data(data, x_ax, y_ax, title, legend):
     x_data, y_data = data_to_xy(data)
+    x_data, y_data = crop_data(x_data, y_data, 1, 1) ################# delete this to draw the uncropped data
 
     plt.errorbar(x_data, y_data, fmt=".", label="", markersize=1, elinewidth=0.2)
     plt.xlabel(x_ax)
@@ -38,14 +39,28 @@ def draw_data(data, x_ax, y_ax, title, legend):
 
 
 def draw_data_and_curve(data, x_ax, y_ax, title, legend, function):
-    xdata, ydata = data_to_xy(data)
-    popt, pcov = curve_fit(function, xdata, ydata,
-                           maxfev=10000)
-    curve_data = function(xdata, popt[0])
-    plt.plot(xdata, curve_data)
+    x_data, y_data = data_to_xy(data)
+    xdata, ydata = crop_data(x_data, y_data, 1, 1)##################### delete this to draw the uncropped data
 
+    popt, pcov = curve_fit(function, xdata, ydata,
+                           maxfev=1000)
+    print("popt", popt)
+    curve_data = function(xdata, popt[0])
+    print("popt", popt)
+    plt.plot(xdata, curve_data)
+    #plt.show()
     draw_data(data, x_ax, y_ax, title, legend) # the show() function is in here
 
 
-def diffraction(phi,I_0):
-    return I_0 * (np.sin(phi) / phi) ** 2
+def crop_data(x_data, y_data, x_start, x_end):
+    crop_x = []
+    crop_y = []
+    for i in range(len(x_data)):
+        if -0.09 <= x_data[i] <= -0.04:
+            crop_x.append(x_data[i])
+            crop_y.append(y_data[i])
+    return crop_x, crop_y
+
+
+def diffraction(x, I):
+    return I * ((np.sin(x)/ x ) ** 2)
