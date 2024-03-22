216133a5faaabbb129f1a8f87ca7122bfab1cf79
Arthur <arthurximing.li@mail.utoronto.ca>
2024-03-22 13:28:08 -0400

    functions.py

--- Interference and Diffracttion/functions.py
+++ Interference and Diffracttion/functions.py
@@ -29,6 +29,7 @@ def data_to_xy(data):
 def draw_data(data, x_ax, y_ax, title, legend):
     x_data, y_data = data_to_xy(data)
     x_data, y_data = crop_data(x_data, y_data, 1, 1) ################# delete this to draw the uncropped data
+    x_data, y_data = rescale_data(x_data, y_data, 1000, 1000)
 
     plt.errorbar(x_data, y_data, fmt=".", label="", markersize=1, elinewidth=0.2)
     plt.xlabel(x_ax)
@@ -41,6 +42,10 @@ def draw_data(data, x_ax, y_ax, title, legend):
 def draw_data_and_curve(data, x_ax, y_ax, title, legend, function):
     x_data, y_data = data_to_xy(data)
     xdata, ydata = crop_data(x_data, y_data, 1, 1)##################### delete this to draw the uncropped data
+    print(x_data[0])
+
+    xdata, ydata = rescale_data(xdata, ydata, 1000, 1000)
+    print(xdata[0])
 
     popt, pcov = curve_fit(function, xdata, ydata,
                            maxfev=1000)
@@ -60,6 +65,12 @@ def crop_data(x_data, y_data, x_start, x_end):
             crop_x.append(x_data[i])
             crop_y.append(y_data[i])
     return crop_x, crop_y
+
+
+def rescale_data(x_data, y_data, x_scale, y_scale):
+    new_x = x_data * x_scale
+    new_y = y_data * y_scale
+    return new_x, new_y
 
 
 def diffraction(x, I):
