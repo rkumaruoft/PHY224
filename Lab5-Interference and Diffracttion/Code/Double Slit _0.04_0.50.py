efaa85267435119431268f3036b9490c64a1f35d
reeshav <reeshav.kumar@mail.utoronto.ca>
2024-04-01 14:37:40 -0400

    Double Slit

--- Lab5-Interference and Diffracttion/Code/Double Slit _0.04_0.50.py
+++ Lab5-Interference and Diffracttion/Code/Double Slit _0.04_0.50.py
@@ -6,11 +6,10 @@ from scipy.optimize import curve_fit
 from functions import *
 
 if __name__ == '__main__':
-    xdata, ydata = data_to_xy("../More Data/Double_slit_0.04_0.25.txt")
-
+    xdata, ydata = data_to_xy("../More Data/Double_slit_0.04_0.50.txt")
     D = 0.706
-
     wavelength = 515 * (10 ** -9)
+
     min_x = abs(min(xdata))
     max_I = abs(max(ydata))
     min_I = abs(min(ydata))
@@ -21,7 +20,7 @@ if __name__ == '__main__':
     x_data_crop = []
     y_data_crop = []
     for index in range(len(xdata)):
-        # if 0.0557 <= xdata[index] <= 0.0733:
+        if 0.055 <= xdata[index] <= 0.071:
             x_data_crop.append(xdata[index])
             y_data_crop.append(ydata[index])
     min_x = abs(min(x_data_crop))
@@ -30,13 +29,12 @@ if __name__ == '__main__':
     ydata = numpy.array(y_data_crop)
     plt.errorbar(xdata, ydata, fmt=".", label="", markersize=1, elinewidth=0.2)
 
-
-    popt, pcov = curve_fit(cos_2, xdata, ydata, p0=[max_I, (numpy.pi * 0.00025 / (wavelength * D)), 0, -0.01])
-    cos_2_data = cos_2(xdata, max_I, popt[1], popt[2], popt[3])
+    popt, pcov = curve_fit(cos_2, xdata, ydata, p0=[max_I, (numpy.pi * 0.00050 / (wavelength * D)), 0, -0.01])
+    cos_2_data = cos_2(xdata, max_I, popt[1], popt[2], -0.01)
     plt.plot(xdata, cos_2_data, alpha=0.2)
 
-
     print(popt[1]*wavelength*D/numpy.pi)
+
 
     plt.xlabel("Location")
     plt.ylabel("Intensity")
--- Lab5-Interference and Diffracttion/Code/Double Slit_0.08_0.25.py
+++ Lab5-Interference and Diffracttion/Code/Double Slit_0.08_0.25.py
@@ -6,11 +6,11 @@ from scipy.optimize import curve_fit
 from functions import *
 
 if __name__ == '__main__':
-    xdata, ydata = data_to_xy("../More Data/Double_slit_0.04_0.25.txt")
+    xdata, ydata = data_to_xy("../More Data/Double_slit_0.08_0.25.txt")
 
     D = 0.706
-
     wavelength = 515 * (10 ** -9)
+
     min_x = abs(min(xdata))
     max_I = abs(max(ydata))
     min_I = abs(min(ydata))
@@ -21,22 +21,23 @@ if __name__ == '__main__':
     x_data_crop = []
     y_data_crop = []
     for index in range(len(xdata)):
-        # if 0.0557 <= xdata[index] <= 0.0733:
+        if 0.061 <= xdata[index] <= 0.07:
             x_data_crop.append(xdata[index])
             y_data_crop.append(ydata[index])
     min_x = abs(min(x_data_crop))
     max_I = abs(max(y_data_crop))
     xdata = numpy.array(x_data_crop)
     ydata = numpy.array(y_data_crop)
+
     plt.errorbar(xdata, ydata, fmt=".", label="", markersize=1, elinewidth=0.2)
 
-
-    popt, pcov = curve_fit(cos_2, xdata, ydata, p0=[max_I, (numpy.pi * 0.00025 / (wavelength * D)), 0, -0.01])
-    cos_2_data = cos_2(xdata, max_I, popt[1], popt[2], popt[3])
+    popt, pcov = curve_fit(cos_2, xdata, ydata, p0=[max_I, (numpy.pi * 0.00025 / (wavelength * D)), 0, -0.01],
+                           maxfev=10000)
+    cos_2_data = cos_2(xdata, max_I, popt[1], popt[2], -0.01)
     plt.plot(xdata, cos_2_data, alpha=0.2)
 
+    print(popt[1] * wavelength * D / numpy.pi)
 
-    print(popt[1]*wavelength*D/numpy.pi)
 
     plt.xlabel("Location")
     plt.ylabel("Intensity")
